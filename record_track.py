3bd55b08d19e329046a7c2cb7c66ee2aca51909e
Pan <gpan@middlebury.edu>
2019-07-17 12:47:02 -0400

    added code for plotting a graph for data, added velocity in data

--- record_track.py
+++ record_track.py
@@ -29,27 +29,29 @@ if interval:
     start_location = np.array(start_point[0:3])
     start_quaternion = Quaternion(start_point[3], start_point[4], start_point[5], start_point[6])
     record = []
-    while(True):
+    while(time.time()-start_time < duration):
         start = time.time()
         current = np.array(v.devices["tracker_1"].get_pose_quaternion())
+        velocity = v.devices["tracker_1"].get_velocity()
         if current.all():
             current_location = current[0:3]
             location_diff = current_location - start_location
             current_quaternion = Quaternion(current[3], current[4], current[5], current[6])
             quaternion_diff = current_quaternion * start_quaternion.inverse
-            record.append([location_diff, quaternion_diff])
-        if (time.time()-start_time > duration):
-            break
+            record.append([location_diff, quaternion_diff, velocity])
         sleep_time = interval - (time.time() - start)
         if sleep_time > 0:
             time.sleep(sleep_time)
     if debug:
         print(record)
-    print("data collection success!")
-    name = input("Enter the name of file:")
-    if not name:
-        name = "test"
-    file_path = open('%s.obj'%name, 'wb')
-    print("saved as %s.obj"%name)
-    pickle.dump(record, file_path)
+    if record:
+        print("data collection success!")
+        name = input("Enter the name of file:")
+        if not name:
+            name = "test"
+        file_path = open('%s.obj'%name, 'wb')
+        print("saved as %s.obj"%name)
+        pickle.dump(record, file_path)
+    else:
+        print("data collection failed")
    
